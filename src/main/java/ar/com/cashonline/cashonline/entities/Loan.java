fd5906c8199a2b3913ce5d3947e5520e3276e669
Sabrina Meli <sabrina.jmeli@gmail.com>
2022-02-10 20:09:44 -0300

    Se crean atributos, con anotaciones getters/setters de ambas entities.

--- src/main/java/ar/com/cashonline/cashonline/entities/Loan.java
+++ src/main/java/ar/com/cashonline/cashonline/entities/Loan.java
@@ -1,5 +1,48 @@
 package ar.com.cashonline.cashonline.entities;
 
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name = "loan")
 public class Loan {
-    
+
+    @Id
+    @Column(name = "loan_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer loanId;
+
+    @Column(name = "total_amount_loan")
+    private Integer totalAmountLoan;
+
+    @ManyToOne
+    @JoinColumn(name="user_id",referencedColumnName = "user_id") 
+    @JsonIgnore
+    private Usuario usuario;
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Integer getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(Integer loanId) {
+        this.loanId = loanId;
+    }
+
+    public Integer getTotalAmountLoan() {
+        return totalAmountLoan;
+    }
+
+    public void setTotalAmountLoan(Integer totalAmountLoan) {
+        this.totalAmountLoan = totalAmountLoan;
+    }
+
 }
--- src/main/java/ar/com/cashonline/cashonline/entities/Usuario.java
+++ src/main/java/ar/com/cashonline/cashonline/entities/Usuario.java
@@ -1,5 +1,92 @@
 package ar.com.cashonline.cashonline.entities;
 
+import java.util.*;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Table(name = "usuario")
 public class Usuario {
-    
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @NaturalId
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstname;
+
+    @Column(name = "last_name")
+    private String lastname;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Loan> loans = new ArrayList<>(); 
+
+    @NaturalId
+    private String username;
+
+    private String password;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastName) {
+        this.lastname = lastName;
+    }
+
+    public List<Loan> getLoans() {
+        return loans;
+    }
+
+    public void setLoans(List<Loan> loans) {
+        this.loans = loans;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
+   
+

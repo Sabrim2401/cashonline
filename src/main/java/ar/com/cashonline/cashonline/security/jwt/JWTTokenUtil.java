4fd0a84293b8947ab39fd7e32674e81d034ed420
Sabrina Meli <sabrina.jmeli@gmail.com>
2022-02-10 20:40:19 -0300

    Se crea el package security con sus clases y metodos.

---
+++ src/main/java/ar/com/cashonline/cashonline/security/jwt/JWTTokenUtil.java
@@ -0,0 +1,105 @@
+package ar.com.cashonline.cashonline.security.jwt;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+/**
+ * Utilidad para generar y validar JWT(Json Web Tokens)
+ */
+@Component
+public class JWTTokenUtil implements Serializable {
+
+    private static final long serialVersionUID = -2550185165626007488L;
+
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+
+    // Clave secreta storeada en el application.properties
+    @Value("${jwt.secret}")
+    private String secret;
+
+    // Obtiene el username del token(o sea el claim subject)
+    public String getUsernameFromToken(String token) {
+
+        return getClaimFromToken(token, Claims::getSubject);
+
+    }
+
+    // obtiene la expiracion del JWT
+    public Date getExpirationDateFromToken(String token) {
+
+        return getClaimFromToken(token, Claims::getExpiration);
+
+    }
+
+    // Obtiene la lista de claims(propositos)
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+
+        final Claims claims = getAllClaimsFromToken(token);
+
+        return claimsResolver.apply(claims);
+
+    }
+
+    // para traer cualquier informacion del token necesitamos la clave secreta
+    public Claims getAllClaimsFromToken(String token) {
+
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+
+    }
+
+    // checkea si el token expiro
+    private Boolean isTokenExpired(String token) {
+
+        final Date expiration = getExpirationDateFromToken(token);
+
+        return expiration.before(new Date());
+
+    }
+
+    // genera un token en base a los datos de la interface UserDetails
+    public String generateToken(UserDetails userDetails) {
+
+        Map<String, Object> claims = new HashMap<>();
+
+        return doGenerateToken(claims, userDetails.getUsername());
+
+    }
+
+    public String generateToken(UserDetails userDetails, Map<String, Object> claims) {
+
+        return doGenerateToken(claims, userDetails.getUsername());
+
+    }
+
+
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+
+    }
+
+    // valida el token
+    public Boolean validateToken(String token, UserDetails userDetails) {
+
+        final String username = getUsernameFromToken(token);
+
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+
+    }
+
+}

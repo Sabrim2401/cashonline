6f9359c022a1be8e24ba89a25908c37f8f87f0c5
Sabrina Meli <sabrina.jmeli@gmail.com>
2022-02-11 11:03:12 -0300

    Se agrega dependencias al pom, se crea package de service, con UsuarioService y LoansService

--- src/main/java/ar/com/cashonline/cashonline/service/LoansService.java
+++ src/main/java/ar/com/cashonline/cashonline/service/LoansService.java
@@ -1,5 +1,118 @@
 package ar.com.cashonline.cashonline.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import ar.com.cashonline.cashonline.entities.Loan;
+import ar.com.cashonline.cashonline.models.request.CrearLoans;
+import ar.com.cashonline.cashonline.models.response.LoansResponse;
+import ar.com.cashonline.cashonline.models.response.PaginationResponse;
+import ar.com.cashonline.cashonline.repos.LoansRepository;
+
+@Service
 public class LoansService {
-    
+
+    @Autowired
+    LoansRepository repo;
+
+    @Autowired
+    UsuarioService serviceUser;
+
+    public void crearLoan(Loan loan) {
+        repo.save(loan);
+    }
+
+    public List<Loan> traerLoans() {
+        return repo.findAll();
+    }
+
+    public Loan buscarPorId(Integer id) {
+        return repo.findByLoanId(id);
+    }
+
+    public List<LoansResponse> findAllLoans() {
+
+        List<Loan> loans = repo.findAll();
+        List<LoansResponse> loansRe = new ArrayList<>(loans.size());
+
+        for (Loan loan : loans) {
+
+            LoansResponse loansResponse = new LoansResponse();
+
+            loansResponse.loanId = loan.getLoanId();
+            loansResponse.total = loan.getTotalAmountLoan();
+            loansResponse.userId = loan.getUsuario().getUserId();
+            loansRe.add(loansResponse);
+        }
+        return loansRe;
+    }
+
+    public Loan crearLoan(CrearLoans loan, String username) {
+
+        var user = serviceUser.buscarPorUsername(username);
+        Loan newLoan = new Loan();
+        newLoan.setTotalAmountLoan(loan.totalAmountLoan);
+        newLoan.setUsuario(user);
+        return repo.save(newLoan);
+
+    }
+
+    public PaginationResponse findAll(Pageable pageable) {
+        Page<Loan> page = repo.findAll(pageable);
+        List<LoansResponse> loanResponse = buildList(page.getContent());
+        return buildPaginationResponse(loanResponse, page);
+    }
+
+    public PaginationResponse findByUsuarioId(Pageable pageable, Integer userId) {
+
+        Page<Loan> page = repo.findAll(pageable);
+
+        List<LoansResponse> loanResponse = buildList(page.getContent());
+
+        List<LoansResponse> loansByUser = new ArrayList<>();
+
+        for (LoansResponse loansResponse : loanResponse) {
+
+            if (loansResponse.userId == userId) {
+                loansByUser.add(loansResponse);
+            }
+
+        }
+
+        return buildPaginationResponse(loansByUser, page);
+    }
+
+    private PaginationResponse buildPaginationResponse(List<LoansResponse> loansResponse,
+            Page<Loan> page) {
+        PaginationResponse paginationResponse = new PaginationResponse();
+        paginationResponse.loansResponse = loansResponse;
+        paginationResponse.page = page.getNumber();
+        paginationResponse.totalPages = page.getTotalPages();
+        paginationResponse.size = page.getSize();
+        return paginationResponse;
+    }
+
+    public List<LoansResponse> buildList(List<Loan> loans) {
+
+        List<LoansResponse> loanR = new ArrayList<>();
+        for (Loan loan : loans) {
+
+            LoansResponse lResponse = new LoansResponse();
+
+            lResponse.total = loan.getTotalAmountLoan();
+            lResponse.userId = loan.getUsuario().getUserId();
+            lResponse.loanId = loan.getLoanId();
+
+            loanR.add(lResponse);
+
+        }
+
+        return loanR;
+    }
+
 }
--- src/main/java/ar/com/cashonline/cashonline/service/UsuarioService.java
+++ src/main/java/ar/com/cashonline/cashonline/service/UsuarioService.java
@@ -1,5 +1,85 @@
 package ar.com.cashonline.cashonline.service;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.authentication.BadCredentialsException;
+
+import ar.com.cashonline.cashonline.entities.Usuario;
+import ar.com.cashonline.cashonline.repos.UsuarioRepository;
+import ar.com.cashonline.cashonline.security.Crypto;
+
+@Service
 public class UsuarioService {
-    
+
+    @Autowired
+    UsuarioRepository repo;
+
+    public Usuario crearUsuario(String email, String firstname, String lastname, String username, String password) {
+        Usuario usuario = new Usuario();
+        usuario.setEmail(email);
+        usuario.setFirstname(firstname);
+        usuario.setLastname(lastname);
+        usuario.setUsername(username);
+        usuario.setPassword(Crypto.encrypt(password, username));
+        return repo.save(usuario);
+    }
+
+    public Usuario buscarPorUsername(String username) {
+        return repo.findByUsername(username);
+    }
+
+    public Usuario findById(Integer id) {
+        return repo.findByUserId(id);
+    }
+
+    public UserDetails getUserAsUserDetail(Usuario usuario) {
+        UserDetails uDetails;
+
+        uDetails = new User(usuario.getUsername(), usuario.getPassword(), getAuthorities(usuario));
+
+        return uDetails;
+    }
+
+    // Usamos el tipo de datos SET solo para usar otro diferente a List private
+    Set<? extends GrantedAuthority> getAuthorities(Usuario usuario) {
+
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+
+        Integer userType = usuario.getUserId();
+
+        authorities.add(new SimpleGrantedAuthority("CLAIM_userType_" + userType.toString()));
+
+        return authorities;
+    }
+
+    public Usuario login(String username, String password) {
+
+        Usuario u = buscarPorUsername(username);
+
+        if (u == null || !u.getPassword().equals(Crypto.encrypt(password, u.getUsername()))) {
+
+            throw new BadCredentialsException("Usuario o contraseña invalida");
+        }
+
+        return u;
+    }
+
+    public Map<String, Object> getUserClaims(Usuario usuario) {
+        Map<String, Object> claims = new HashMap<>();
+
+        claims.put("userType", usuario.getUserId()); ///
+
+        return claims;
+    }
+
+    public void bajaUsuarioPorId(Integer id) {
+        repo.deleteById(id);
+    }
+
 }
